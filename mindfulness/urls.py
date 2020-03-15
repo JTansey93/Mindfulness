3f876a9205e044156ee8da4769962adc1398a046
James <57265750+JTansey93@users.noreply.github.com>
2020-03-15 16:19:26 +0000

    Added base template, along with files for the home and about page

--- mindfulness/urls.py
+++ mindfulness/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import public.views as public_views
 
 urlpatterns = [
+    path('', public_views.home, name='home'),
+    path('about/', public_views.about, name='about'),
     path('admin/', admin.site.urls),
 ]
