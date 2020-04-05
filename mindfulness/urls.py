fd9dd7bdf1ef1c84479b4bb49f7c321260f22d99
James <57265750+JTansey93@users.noreply.github.com>
2020-04-05 15:10:48 +0100

    Added the routes for logging users in and out

--- mindfulness/urls.py
+++ mindfulness/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('', public_views.home, name='home'),
     path('about/', public_views.about, name='about'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('admin/', admin.site.urls),
 ]
